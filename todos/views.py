3068e63a8c06addd69d3c8485ba08209f140e40f
rkpust <rezaul.cse.pust17@gmail.com>
2023-09-07 12:08:37 +0600

    added path for todos app

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def list_todo_items(request):
+    return HttpResponse('From list_todo_items')
