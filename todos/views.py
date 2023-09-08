65e4ed6376e7adada81dfdcf36ba4d4dfc67ef98
rkpust <rezaul.cse.pust17@gmail.com>
2023-09-08 20:43:48 +0600

    showed todo item in template

--- todos/views.py
+++ todos/views.py
@@ -5,7 +5,8 @@ from .models import ToDo
 # Create your views here.
 
 def list_todo_items(request):
-    return render(request, 'todos/todo_list.html')
+    context = { 'todo_list': ToDo.objects.all() }
+    return render(request, 'todos/todo_list.html', context)
 
 def insert_todo_item(request:HttpRequest):
     todo = ToDo(content = request.POST['content'])
