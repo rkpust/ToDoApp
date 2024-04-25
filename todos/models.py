26e0f3e8bc2785037aeb0eb0b6e3ae0058dd5b34
rkpust <rezaul.cse.pust17@gmail.com>
2024-04-25 20:01:57 +0600

    added multi-row data submit by clicking a button

--- todos/models.py
+++ todos/models.py
@@ -3,3 +3,11 @@ from django.db import models
 # Create your models here.
 class ToDo(models.Model):
     content = models.TextField()
+    status = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.content}'
+
+    class Meta:
+        verbose_name_plural = "ToDos"
+        # db_table = 'ToDos'
--- todos/urls.py
+++ todos/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('list/', views.list_todo_items),
     path('insert_todo/', views.insert_todo_item),
+    path('update_todos_status/', views.update_selected_todo_status, name='update_selected_todo_status'),
     path('delete_todo/<int:todo_id>/', views.delete_todo_item, name='delete_todo_item')
 ]
